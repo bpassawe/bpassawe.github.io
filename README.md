24e516ec5548604591266b8dcf99930f79f04918
bpassawe <17372657+bpassawe@users.noreply.github.com>
2023-05-30 00:46:56 +0100

    Complete one-page website and add responsiveness to site

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Bill Passawe's Portfolio
 
-This project is about building my portfolio to show all of my projects I have built with during my training with Codecademy. 
+This is a portfolio project to show some of my projects I have worked on. This project was mainly setup so that I could easily allow potential employers or clients to find out who I am and what I can do.
+
+How to Use?
+-----------
+
+You will have to download and unzip the entire directory to your computer, then double click the the index.html file to open the site. If you're asked to choose a browser, select the browser of your choice to open the site.
+
+
+## Technologies 
+
+This project was built with the following programming languages/scripts:
+
+- HTML
+- CSS
+- JavaScript
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// This function will show or hide the site menu when the icon bar is clicked
+// call-back function to show/hide main menu on page
 const showMenu = () => {
     const menuLinks = document.querySelector(".nav-content");
     const button = document.querySelector(".btn-nav");
@@ -6,7 +6,6 @@ const showMenu = () => {
     if (button.classList.contains("animated")) {
         button.classList.remove("animated");
         menuLinks.classList.remove("showNav");
-        menuLinks.classList.add("hidden");
         menuLinks.classList.add("hideNav");
     } else {
         button.classList.add("animated");
@@ -16,6 +15,68 @@ const showMenu = () => {
     }
 };
 
-// Set menu button to listen for click event
+// button set to listen for click event
 const toggleButton = document.querySelector(".nav");
 toggleButton.addEventListener("click", showMenu);
+
+
+// Target all clicks on any element
+document.addEventListener('click', (e) => 
+{
+    // Get button elements
+    const showAboutContent = document.querySelector(".about-text-box");
+    const buttonAbout = document.querySelector(".about-header-box");
+    const projectHeader = document.querySelector(".project-header-box");
+    const projectContent = document.querySelector(".project-content-box");
+
+    // Get element class(es)
+    let elementClass = e.target.className;
+
+    // Show and scroll to the about section on the page
+    switch (elementClass) {
+        case 'open-about':
+        case 'open-about-tag':
+            if (showAboutContent.classList.contains("hidden")) {
+                showAboutContent.classList.add("shown");
+                showAboutContent.classList.remove("hidden");
+                showAboutContent.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+            } else {
+                showAboutContent.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+            }
+            break;
+        case 'close-about':
+        case 'close-about-tag':
+            buttonAbout.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+            showAboutContent.classList.remove("shown");
+            showAboutContent.classList.add("hidden");
+            break;
+        case 'open-project':
+        case 'open-project-tag':
+            if (projectContent.classList.contains("hidden")) {
+                projectContent.classList.add("shown");
+                projectContent.classList.remove("hidden");
+                projectContent.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+            } else {
+                projectContent.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+            }
+            break;
+        case 'close-project':
+        case 'close-project-tag':
+            projectHeader.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+            projectContent.classList.remove("shown");
+            projectContent.classList.add("hidden");
+            break;
+    }
+});
+
+// Get references to primary links
+const homeLink= document.getElementById("homeLink");
+const aboutLink= document.getElementById("aboutLink");
+const projectLink= document.getElementById("projectLink");
+const contactLink= document.getElementById("contactLink");
+
+// Hide the primary menu and go to section of page linked to
+homeLink.addEventListener('click', showMenu);
+aboutLink.addEventListener('click', showMenu);
+projectLink.addEventListener('click', showMenu);
+contactLink.addEventListener('click', showMenu);
